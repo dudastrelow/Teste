5ffa535041e08eaf3f638e6e3d6638badda418e5
dudastrelow <maria.strelow@estudante.ifro.edu.br>
2023-10-05 15:05:20 -0400

    Primeiro commit

---
+++ Teste/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um numero: ");
+        double n = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite outro numero: ");
+        double n2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"Soma é igual a:  {n + n2} ");
+
+        Console.ReadKey();
+    }
+}
